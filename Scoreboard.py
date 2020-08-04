c6be58482da79f369b4ba3c29f0d49e9f04d3590
Rupert <rupi.wildner@gmx.at>
2020-08-05 00:40:03 +0200

    .

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,9 +1,11 @@
 
 from tkinter import *
 import tkinter.ttk as ttk
-import requests
+
 from bs4 import BeautifulSoup
 import functools
+
+import grequests
 
 class Display:
 
@@ -386,66 +388,63 @@ class Einstellungen:
     def __init__(self,screen_einstellungen):
         self.master = screen_einstellungen
         aufloesungen = ["1920x200",]
-        data=[] #nichts drinnen
         self.teams = []
         self.teams_player = []
-
-        self.master.geometry("670x330")
-        self.master.title("Bierpong Scoreboard - Einstellungen")
         
-
-        self.lb_resolution = Label(screen_einstellungen, text="Auflösung")
+        self.master.geometry("670x330")
+        self.master.title("Bierpong Scoreboard - Einstellungen")        
+        self.lb_resolution = Label(self.master, text="Auflösung")
         self.lb_resolution.place(relx=0.015, rely=0.2)
-        self.cb_resolution = ttk.Combobox(screen_einstellungen, value=aufloesungen, state='readonly')
+        self.cb_resolution = ttk.Combobox(self.master, value=aufloesungen, state='readonly')
         self.cb_resolution.place(relx=0.2, rely=0.2)
     
 
-        self.lb_hometeam = Label(screen_einstellungen, text="Heimteam")
+        self.lb_hometeam = Label(self.master, text="Heimteam")
         self.lb_hometeam.place(relx=0.015, rely=0.3)
-        self.cb_hometeam = ttk.Combobox(screen_einstellungen, value=self.teams, postcommand = self.updateCB)
+        self.cb_hometeam = ttk.Combobox(self.master, value=self.teams, postcommand = self.updateCB)
         self.cb_hometeam.place(relx=0.2, rely=0.3)
 
 
-        self.lb_awayteam = Label(screen_einstellungen, text="Auswärtsteam")
+        self.lb_awayteam = Label(self.master, text="Auswärtsteam")
         self.lb_awayteam.place(relx=0.015, rely=0.4)
-        self.cb_awayteam = ttk.Combobox(screen_einstellungen, value=self.teams, postcommand = self.updateCB)
+        self.cb_awayteam = ttk.Combobox(self.master, value=self.teams, postcommand = self.updateCB)
         self.cb_awayteam.place(relx=0.2, rely=0.4)
 
 
-        self.lb_playerhome = Label(screen_einstellungen, text="Spieler-Heim")
+        self.lb_playerhome = Label(self.master, text="Spieler-Heim")
         self.lb_playerhome.place(relx=0.54, rely=0.3)
-        self.cb_playerhome = ttk.Combobox(screen_einstellungen, postcommand= self.updateCB_Home)
+        self.cb_playerhome = ttk.Combobox(self.master, postcommand= self.updateCB_Home)
         self.cb_playerhome.place(relx=0.75, rely=0.3)
 
 
-        self.lb_playeraway = Label(screen_einstellungen, text="Spieler-Auswärts")
+        self.lb_playeraway = Label(self.master, text="Spieler-Auswärts")
         self.lb_playeraway.place(relx=0.54, rely=0.4)
-        self.cb_playeraway = ttk.Combobox(screen_einstellungen, postcommand = self.updateCB_Away)
+        self.cb_playeraway = ttk.Combobox(self.master, postcommand = self.updateCB_Away)
         self.cb_playeraway.place(relx=0.75, rely=0.4)
 
 
-        self.bt_getdata = ttk.Button(screen_einstellungen, text="Lade Daten", command=self.getdata)
+        self.bt_getdata = ttk.Button(self.master, text="Lade Daten", command=self.getdata)
         self.bt_getdata.place(relx=0.55, rely=0.15, height=35, width=290)
         
-        self.bt_newgame = ttk.Button(screen_einstellungen, text="New Game", command=self.newGame )
+        self.bt_newgame = ttk.Button(self.master, text="New Game", command=self.newGame )
         self.bt_newgame.place(relx=0.45, rely=0.75,relwidth=0.3)
 
-        self.sep_setting = ttk.Separator(screen_einstellungen)
+        self.sep_setting = ttk.Separator(self.master)
         self.sep_setting.place(relx=0.333, rely=0.666, relwidth=0.333)
 
         self.var_double = IntVar()
-        self.bt_doubles = Checkbutton(screen_einstellungen, text="Doppel", variable = self.var_double, onvalue=1, offvalue=0 , command=self.checkDoubles)
+        self.bt_doubles = Checkbutton(self.master, text="Doppel", variable = self.var_double, onvalue=1, offvalue=0 , command=self.checkDoubles)
         self.bt_doubles.place(relx=0.2, rely=0.5)
 
-        self.lb_playerhome_2 = Label(screen_einstellungen, text="Spieler-Heim-Doppel")
+        self.lb_playerhome_2 = Label(self.master, text="Spieler-Heim-Doppel")
         self.lb_playerhome_2.place(relx=0.54, rely=0.5)
-        self.cb_playerhome_2 = ttk.Combobox(screen_einstellungen, postcommand= self.updateCB_Home, state=DISABLED)
+        self.cb_playerhome_2 = ttk.Combobox(self.master, postcommand= self.updateCB_Home, state=DISABLED)
         self.cb_playerhome_2.place(relx=0.75, rely=0.5)
 
 
-        self.lb_playeraway_2 = Label(screen_einstellungen, text="Spieler-Auswärts-Doppel")
+        self.lb_playeraway_2 = Label(self.master, text="Spieler-Auswärts-Doppel")
         self.lb_playeraway_2.place(relx=0.54, rely=0.6)
-        self.cb_playeraway_2 = ttk.Combobox(screen_einstellungen, postcommand = self.updateCB_Away, state=DISABLED)
+        self.cb_playeraway_2 = ttk.Combobox(self.master, postcommand = self.updateCB_Away, state=DISABLED)
         self.cb_playeraway_2.place(relx=0.75, rely=0.6)
 
 
@@ -456,22 +455,48 @@ class Einstellungen:
         else:
             self.cb_playerhome_2.configure(state=DISABLED)
             self.cb_playeraway_2.configure(state=DISABLED)
+            self.cb_playeraway_2.set("")
+            self.cb_playerhome_2.set("")
 
     def getdata(self):
         self.teams.clear()
-        response = requests.get("https://bpbl.de/tabelle/")
-        soup = BeautifulSoup(response.text, 'lxml')
+        domains = ["https://bpbl.de/tabelle/"]
+        req = (grequests.get(u) for u in domains)
+        response = grequests.map(req)
+        soup = BeautifulSoup(response[0].text, 'lxml')
         table = soup.find('table',{'class':'sp-league-table'})
-
-        for href in table.find_all('a'):
-            players = []
-            self.teams.append(href.getText())
-            #players.append(href.getText()) #teamname
-            #print (href.get('href'))
-            tmpurl = href.get('href')
+        
+        # for href in table.find_all('a'):
+        #     players = []
+        #     self.teams.append(href.getText())
+        #     #players.append(href.getText()) #teamname
+        #     #print (href.get('href'))
+        #     tmpurl = href.get('href')
             
-            tmp_response = requests.get(tmpurl)
-            soup = BeautifulSoup(tmp_response.text, 'lxml')
+        #     tmp_response = requests.get(tmpurl)
+        #     soup = BeautifulSoup(tmp_response.text, 'lxml')
+        #     tmp_table =  soup.find('table',{'class':'sp-player-list'})
+        #     for player in tmp_table.find_all('a'):
+        #         #clean female players from ♀
+        #         tmp_player = player.getText()
+        #         new_player = tmp_player.replace('♀',"")        
+        #         players.append(new_player)
+        #     #print (players)
+        #     self.teams_player.append(players.copy())
+
+        l_teamurls = []
+        l_requests = []
+        for href in table.find_all('a'):
+            #fill team list
+            self.teams.append(href.getText())
+            l_teamurls.append(href.get('href'))
+        for i in l_teamurls:
+            l_requests.append(grequests.get(i))
+            
+        responses = grequests.map(l_requests)
+        for resp in responses:
+            players = []
+            soup = BeautifulSoup(resp.text, 'lxml')
             tmp_table =  soup.find('table',{'class':'sp-player-list'})
             for player in tmp_table.find_all('a'):
                 #clean female players from ♀
@@ -481,6 +506,9 @@ class Einstellungen:
             #print (players)
             self.teams_player.append(players.copy())
 
+
+
+        
     def newGame(self):
         #TODO implementierung doubles
         self.win = Toplevel(self.master)
@@ -518,10 +546,6 @@ def main():
     root = Tk()
     app = Einstellungen(root)
     root.mainloop()
-
-
-
-
 
 
 
